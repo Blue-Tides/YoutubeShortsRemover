1e77883a4d628606d51614b9f560032b9b755777
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-03-29 19:02:37 -0700

    added privacy policy

--- background.js
+++ background.js
@@ -5,8 +5,6 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.tabs.onUpdated.addListener(
     (a,b,c)=>{
         if (c["url"].slice(0,31)=="https://www.youtube.com/shorts/") {
-            console.log(c.url)
-            console.log("https://www.youtube.com/watch?v="+c["url"].slice(31))
             chrome.tabs.update(
                 a,
                 {"url":"https://www.youtube.com/watch?v="+c["url"].slice(31)},
